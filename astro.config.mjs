9b1051e113bf127ff1f26b86ae3ee0d3b631e4a5
Devansh Jain <devanshjain131@gmail.com>
2026-06-04 01:23:39 +0530

    some change

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,14 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://tripspackinglist.com',
-  integrations: [sitemap()],
+  integrations: [sitemap({
+    serialize(item) {
+      if (/\/400\/?$/.test(item.url)) {
+        return undefined;
+      }
+      return item;
+    }
+  })],
   build: {
     inlineStylesheets: 'always'
   },
